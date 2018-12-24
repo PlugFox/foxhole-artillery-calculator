0bb4b2393a7297cdd762b3449777843f2f550ede
Plague Fox <plugfox@gmail.com>
2018-12-24 17:59:17 +0300

    v1.2

--- foxhole-artillery-calculator/MainWindow.xaml.cs
+++ foxhole-artillery-calculator/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using foxhole_artillery_calculator.classes;
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Media;
 using System.Drawing;
@@ -11,11 +10,16 @@ using System.Speech.Synthesis;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Globalization;
+using foxhole_artillery_calculator.classes;
+using foxhole_artillery_calculator.screens;
 
 namespace foxhole_artillery_calculator
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    ///  
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -30,7 +34,15 @@ namespace foxhole_artillery_calculator
         // Количество введенных символов в текущее поле
         private int count = 0;
         // Состояние текущего ввода
-        private Status currentStatus;
+        private Status currentStatus {
+            get => _currentStatus;
+            set
+            {
+                //Task.Run(() => Console.Beep(300, 200));
+                _currentStatus = value;
+            }
+        }
+        private Status _currentStatus;
         private enum Status {
             none,
             enemyDistance,
@@ -42,7 +54,6 @@ namespace foxhole_artillery_calculator
         private static readonly Regex regex = new Regex(@"^\d$");
         // Создадим клавиатурный хук
         KeyboardHook keyboardHook = new KeyboardHook();
-
         // Перемещение окна
         [DllImport("user32.dll", SetLastError = true)]
         internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
@@ -80,60 +91,73 @@ namespace foxhole_artillery_calculator
         // Обновить интерфейс
         void UpdateInterface()
         {
-            // Установлю значения меток из переменных
-            enemyDistanceLBL.Content = enemyDistance;
-            enemyAzimuthLBL.Content = enemyAzimuth;
-            friendlyDistanceLBL.Content = friendlyDistance;
-            friendlyAzimuthLBL.Content = friendlyAzimuth;
-
-            // Посчитаю координаты цели
-            Calculate();
-            targetDistanceLBL.Content = targetDistance;
-            targetAzimuthLBL.Content = targetAzimuth;
-
-            // Установлю цвет полей в зависимости от текущего статуса
-            enemyDistanceLBL.Background = new SolidColorBrush(Colors.Transparent);
-            enemyAzimuthLBL.Background = new SolidColorBrush(Colors.Transparent);
-            friendlyDistanceLBL.Background = new SolidColorBrush(Colors.Transparent);
-            friendlyAzimuthLBL.Background = new SolidColorBrush(Colors.Transparent);
-            switch (currentStatus)
+            this.Dispatcher.Invoke(delegate
             {
-                case Status.enemyDistance:
-                    enemyDistanceLBL.Background = new SolidColorBrush(Colors.Lime);
-                    break;
-                case Status.enemyAzimuth:
-                    enemyAzimuthLBL.Background = new SolidColorBrush(Colors.Lime);
-                    break;
-                case Status.friendlyDistance:
-                    friendlyDistanceLBL.Background = new SolidColorBrush(Colors.Lime);
-                    break;
-                case Status.friendlyAzimuth:
-                    friendlyAzimuthLBL.Background = new SolidColorBrush(Colors.Lime);
-                    break;
-                default:
-                    break;
-            }
+                // Установлю значения меток из переменных
+                enemyDistanceLBL.Content = enemyDistance;
+                enemyAzimuthLBL.Content = enemyAzimuth;
+                friendlyDistanceLBL.Content = friendlyDistance;
+                friendlyAzimuthLBL.Content = friendlyAzimuth;
 
+                // Посчитаю координаты цели
+                Calculate();
+                targetDistanceLBL.Content = targetDistance;
+                targetAzimuthLBL.Content = targetAzimuth;
+
+                // Установлю цвет полей в зависимости от текущего статуса
+                enemyDistanceLBL.Background = new SolidColorBrush(Colors.Transparent);
+                enemyAzimuthLBL.Background = new SolidColorBrush(Colors.Transparent);
+                friendlyDistanceLBL.Background = new SolidColorBrush(Colors.Transparent);
+                friendlyAzimuthLBL.Background = new SolidColorBrush(Colors.Transparent);
+                switch (currentStatus)
+                {
+                    case Status.enemyDistance:
+                        enemyDistanceLBL.Background = new SolidColorBrush(Colors.Lime);
+                        break;
+                    case Status.enemyAzimuth:
+                        enemyAzimuthLBL.Background = new SolidColorBrush(Colors.Lime);
+                        break;
+                    case Status.friendlyDistance:
+                        friendlyDistanceLBL.Background = new SolidColorBrush(Colors.Lime);
+                        break;
+                    case Status.friendlyAzimuth:
+                        friendlyAzimuthLBL.Background = new SolidColorBrush(Colors.Lime);
+                        break;
+                    default:
+                        break;
+                }
+            });
             return;
         }
 
+        // Нажатие кнопки таблиц
+        private void Button_Data(object sender, RoutedEventArgs e) => (new Data()).ShowDialog();
+
+        // Нажатие кнопки информации о программе
+        private void Button_Info(object sender, RoutedEventArgs e) => (new Info()).ShowDialog();
+
         // Нажатие кнопки свернуть/развернуть
-        private void Button_Turn(object sender, RoutedEventArgs e)
-        {
-            body.Visibility = body.IsVisible ? Visibility.Collapsed : Visibility.Visible;
-        }
+        private void Button_Turn(object sender, RoutedEventArgs e) => Dispatcher.Invoke(() => body.Visibility = body.IsVisible ? Visibility.Collapsed : Visibility.Visible);
 
         // Нажатие кнопки выход
-        private void Button_Exit(object sender, RoutedEventArgs e) => Close();
+        private void Button_Exit(object sender, RoutedEventArgs e) => System.Windows.Application.Current.Shutdown();
+
+        // Нажатие на скриншот убирает его
+        private void Screenshot_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) => Screenshot.Source = null;
 
         // Сохранение координат
         private void Label_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             if (e == null)
                 return;
-            ContentControl s = ((ContentControl)sender);
-            s.Content = (targetDistance != 0 || targetAzimuth != 0) ? ("" + targetDistance + Environment.NewLine + targetAzimuth) : ("");
+            Task.Run(() =>
+            {
+                ContentControl s = ((ContentControl)sender);
+                Dispatcher.Invoke(() => s.Content = (targetDistance != 0 || targetAzimuth != 0) ? ("" + targetDistance + Environment.NewLine + targetAzimuth) : ("") );
+            });
         }
+        // Нажатие кнопки озвучки координат
+        private void Sound_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) => Say(targetDistance, targetAzimuth);
         #endregion
 
         #region Считаю координаты цели
@@ -171,19 +195,21 @@ namespace foxhole_artillery_calculator
 
         #region Чтение клавиатуры
         // Передадим нажатую клавишу клавиатуры
-        private void KeyboardHook_KeyDown(KeyboardHook.VKeys key)
-        {
-            string value = key.ToString().ToLower();
-            switch (value)
+        private void KeyboardHook_KeyDown(KeyboardHook.VKeys VKey) => KeyPressedAsync(VKey);
+        private async void KeyPressedAsync(KeyboardHook.VKeys VKey) => await Task.Run(() => KeyPressed(VKey));
+        private void KeyPressed(KeyboardHook.VKeys VKey)
+        {            
+            switch (VKey)
             {
                 // Если нажали на минус и хотим установить позицию врага
-                case "subtract":
+                case KeyboardHook.VKeys.SUBTRACT:
                     count = 0;
                     enemyAzimuth = 0;
                     if (currentStatus == Status.enemyDistance)
                     {
                         currentStatus = Status.enemyAzimuth;
-                    } else
+                    }
+                    else
                     {
                         currentStatus = Status.enemyDistance;
                         enemyDistance = 0;
@@ -191,7 +217,7 @@ namespace foxhole_artillery_calculator
                     UpdateInterface();
                     return;
                 // Если нажали на плюс и хотим установить позицию союзника
-                case "add":
+                case KeyboardHook.VKeys.ADD:
                     count = 0;
                     friendlyAzimuth = 0;
                     if (currentStatus == Status.friendlyDistance)
@@ -206,18 +232,24 @@ namespace foxhole_artillery_calculator
                     UpdateInterface();
                     return;
                 // Если нажали на принтскрин и заскринили текущие координаты
-                case "snapshot":
+                case KeyboardHook.VKeys.SNAPSHOT:
                     CaptureScreen();
                     return;
                 // Если нажали на умножение - меняем разрешение окна фоксхола на квадратное
-                case "multiply":
+                case KeyboardHook.VKeys.MULTIPLY:
                     ChangeResolution();
+                    return;
+                // Если нажали на точку - свернуть/развернуть окно
+                case KeyboardHook.VKeys.DECIMAL:
+                    Button_Turn(null, null);
                     return;
                 default:
                     if (currentStatus == Status.none)
-                        return;               
+                        return;
                     break;
             }
+
+            string value = VKey.ToString().ToLower();
 
             // Если нажали не на Num -, Num +, PrtScr и не на Num 0-9 
             // или текущий ввод не активен - прерываем любые вводы и завершаем процедуру
@@ -260,7 +292,8 @@ namespace foxhole_artillery_calculator
                     enemyDistance += digit;
                     // Если уже ввели 3 цифры в текущее поле 
                     // (таким образом учитываем и лидирующие нули)
-                    if (count == 3)
+                    // Или если ввели 2 цифры и лидирующая цифра больше 1
+                    if (count == 3 || (count == 2 && enemyDistance > 9 && Math.Truncate((double)enemyDistance/10) > 1))
                     {
                         count = 0;
                         currentStatus = Status.enemyAzimuth;
@@ -271,7 +304,8 @@ namespace foxhole_artillery_calculator
                     enemyAzimuth += digit;
                     // Если уже ввели 3 цифры в текущее поле 
                     // (таким образом учитываем и лидирующие нули)
-                    if (count == 3)
+                    // Или если ввели 2 цифры и лидирующая цифра больше 3
+                    if (count == 3 || (count == 2 && enemyAzimuth > 9 && Math.Truncate((double)enemyAzimuth / 10) > 3))
                     {
                         count = 0;
                         currentStatus = Status.none;
@@ -282,7 +316,8 @@ namespace foxhole_artillery_calculator
                     friendlyDistance += digit;
                     // Если уже ввели 3 цифры в текущее поле 
                     // (таким образом учитываем и лидирующие нули)
-                    if (count == 3)
+                    // Или если ввели 2 цифры и лидирующая цифра больше 1
+                    if (count == 3 || (count == 2 && friendlyDistance > 9 && Math.Truncate((double)friendlyDistance / 10) > 1))
                     {
                         count = 0;
                         currentStatus = Status.friendlyAzimuth;
@@ -293,7 +328,8 @@ namespace foxhole_artillery_calculator
                     friendlyAzimuth += digit;
                     // Если уже ввели 3 цифры в текущее поле 
                     // (таким образом учитываем и лидирующие нули)
-                    if (count == 3)
+                    // Или если ввели 2 цифры и лидирующая цифра больше 3
+                    if (count == 3 || (count == 2 && friendlyAzimuth > 9 && Math.Truncate((double)friendlyAzimuth / 10) > 3))
                     {
                         count = 0;
                         currentStatus = Status.none;
@@ -308,94 +344,113 @@ namespace foxhole_artillery_calculator
         #region Сделать скриншот
         private void CaptureScreen()
         {
-            int w = 100;
-            int h = 50;
-            var pos = System.Windows.Forms.Cursor.Position;
-            Bitmap result = new Bitmap(w, h);
-
-            try
+            Task.Run(() =>
             {
-                using (Graphics g = Graphics.FromImage(result))
+                int w = 100;
+                int h = 50;
+                var pos = System.Windows.Forms.Cursor.Position;
+                Bitmap result = new Bitmap(w, h);
+
+                try
                 {
-                    Rectangle rectangle = new Rectangle(0, 0, w, h);
-                    g.CopyFromScreen(pos.X-w/2, pos.Y+10, 0, 0, rectangle.Size, CopyPixelOperation.SourceCopy);
+                    using (Graphics g = Graphics.FromImage(result))
+                    {
+                        Rectangle rectangle = new Rectangle(0, 0, w, h);
+                        g.CopyFromScreen(pos.X-w/2, pos.Y+10, 0, 0, rectangle.Size, CopyPixelOperation.SourceCopy);
+                    }
                 }
-            }
-            catch
-            {
-                return;
-            }
+                catch
+                {
+                    return;
+                }
 
-            using (MemoryStream memory = new MemoryStream())
-            {
-                result.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-                memory.Position = 0;
-                BitmapImage bitmapimage = new BitmapImage();
-                bitmapimage.BeginInit();
-                bitmapimage.StreamSource = memory;
-                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapimage.EndInit();
+                this.Dispatcher.Invoke(delegate
+                {
+                    using (MemoryStream memory = new MemoryStream())
+                    {
+                        result.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                        memory.Position = 0;
+                        BitmapImage bitmapimage = new BitmapImage();
+                        bitmapimage.BeginInit();
+                        bitmapimage.StreamSource = memory;
+                        bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapimage.EndInit();
 
-                Screenshot.Source = bitmapimage;
-            }
+                        Screenshot.Source = bitmapimage;
+                    }
+                });
+            });
         }
         #endregion
 
         #region Сменить разрешение экрана
         void ChangeResolution()
         {
-
-            //IntPtr handle = FindWindow(null, "War");
-
-            Process[] processes = Process.GetProcesses();
-            foreach (var process in processes)
+            Task.Run(() =>
             {
-                if (!process.ProcessName.StartsWith("War") || process.MainWindowHandle == IntPtr.Zero)
-                    continue;
-                IntPtr handle = process.MainWindowHandle;
-
-                int X;
-                int Y;
-                int nWidth;
-                int nHeight;
-
-                switch (currentMode)
+                Process[] processes = Process.GetProcesses();
+                foreach (var process in processes)
                 {
-                    case Mode.Square:
-                        currentMode = Mode.FullScreen;
-                        nWidth = screenWidth;
-                        nHeight = screenHeight;
-                        X = 0;
-                        Y = 0;
-                        break;
-                    case Mode.FullScreen:
-                    default:
-                        currentMode = Mode.Square;
-                        nWidth = screenHeight * 4 / 3;
-                        nHeight = screenHeight;
-                        X = (screenWidth - screenHeight * 4 / 3) / 2;
-                        Y = 0;
-                        break;
+                    if (!process.ProcessName.StartsWith("War") || process.MainWindowHandle == IntPtr.Zero)
+                        continue;
+                    IntPtr handle = process.MainWindowHandle;
+
+                    int X;
+                    int Y;
+                    int nWidth;
+                    int nHeight;
+
+                    switch (currentMode)
+                    {
+                        case Mode.Square:
+                            currentMode = Mode.FullScreen;
+                            nWidth = screenWidth;
+                            nHeight = screenHeight;
+                            X = 0;
+                            Y = 0;
+                            break;
+                        case Mode.FullScreen:
+                        default:
+                            currentMode = Mode.Square;
+                            nWidth = screenHeight * 4 / 3;
+                            nHeight = screenHeight;
+                            X = (screenWidth - screenHeight * 4 / 3) / 2;
+                            Y = 0;
+                            break;
+                    }
+                    MoveWindow(hWnd: handle, X: X, Y: Y, nWidth: nWidth, nHeight: nHeight, bRepaint: true);
+                    SetForegroundWindow(handle);
                 }
-                MoveWindow(hWnd: handle, X: X, Y: Y, nWidth: nWidth, nHeight: nHeight, bRepaint: true);
-                SetForegroundWindow(handle);
-            }
+            });
         }
         #endregion
 
         #region Синтез речи
-        //void Say()
-        //{ 
-        //    // Initialize a new instance of the speech synthesizer.  
-        //    using (SpeechSynthesizer synth = new SpeechSynthesizer())  
-        //    {  
-        //        // Configure the synthesizer to send output to the default audio device.  
-        //        synth.SetOutputToDefaultAudioDevice();  
+        void Say(int sayDistance, int sayAzimuth = 0)
+        {
+            if (sayDistance == 0)
+                return;
+            Task.Run(() =>
+            {
+                try { 
+                    // Initialize a new instance of the speech synthesizer.  
+                    using (SpeechSynthesizer synth = new SpeechSynthesizer())
+                    {
+                        // Configure the synthesizer to send output to the default audio device.  
+                        synth.SetOutputToDefaultAudioDevice();
 
-        //        // Speak a phrase.  
-        //        synth.Speak("142 135");  
-        //    }
-        //}
+                        synth.SelectVoiceByHints(VoiceGender.Neutral, VoiceAge.NotSet, 0, CultureInfo.GetCultureInfo("en-US"));
+
+                        // Speak a phrase.  
+                        synth.Speak(String.Format("Distance: {0}, azimuth: {1}", sayDistance, sayAzimuth));
+                    }
+                } finally
+                {
+
+                }
+            });
+        }
         #endregion
+    
     }
 }
---
+++ foxhole-artillery-calculator/screens/Data.xaml.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace foxhole_artillery_calculator.screens
+{
+    /// <summary>
+    /// Логика взаимодействия для Data.xaml
+    /// </summary>
+    public partial class Data : Window
+    {
+        private const string content  = @"<html>
+        <head>
+        <meta http-equiv=Content-Type content='text/html;'>
+        <style>
+            body {
+                background: black;
+                color: darkorange;
+            }
+            table, tbody, th, td {
+                border: 1px solid #333;
+            }
+        </style>
+        </head>
+        <body scroll='no'>
+        <table cellpadding=0 cellspacing=0 width=480 style='border-collapse:collapse;table-layout:fixed;width:360pt'>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt;width:135pt;text-align: center;'>Name</td>
+          <td style='width:56pt;text-align: center;'>Health</td>
+          <td style='width:56pt;text-align: center;'>Mortar</td>
+          <td style='width:56pt;text-align: center;'>Artillery</td>
+          <td style='width:56pt;text-align: center;'>Howitzer</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Storage box</td>
+          <td>1500</td>
+          <td>2</td>
+          <td>1</td>
+          <td>1</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Barracks</td>
+          <td>1500</td>
+          <td>2</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Encampment</td>
+          <td>?</td>
+          <td>4</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Field Base</td>
+          <td>?</td>
+          <td>8</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Outpost</td>
+          <td>11500</td>
+          <td>11</td>
+          <td>?</td>
+          <td>50</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Fort</td>
+          <td>?</td>
+          <td>13</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Garrison Camp</td>
+          <td>?</td>
+          <td>7</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Garrison Base</td>
+          <td>?</td>
+          <td>?</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Town Hall</td>
+          <td>20000</td>
+          <td>?</td>
+          <td>6</td>
+          <td>100</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Chain Link Fence</td>
+          <td>500</td>
+          <td>1</td>
+          <td>1</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Sandbags</td>
+          <td>1500</td>
+          <td>2</td>
+          <td>1</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Reinforced Wall</td>
+          <td>3000</td>
+          <td>3</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Heavy Gate</td>
+          <td>8500</td>
+          <td>9</td>
+          <td>2</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Fortress Wall</td>
+          <td>16500</td>
+          <td>17</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Foxhole</td>
+          <td>1500</td>
+          <td>2</td>
+          <td>1</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Gun Nest</td>
+          <td>3000</td>
+          <td>3</td>
+          <td>2</td>
+          <td>8</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Gun Turret</td>
+          <td>2500</td>
+          <td>3</td>
+          <td>2</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Light Pillbox</td>
+          <td>?</td>
+          <td>3</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Pillbox</td>
+          <td>4500</td>
+          <td>5</td>
+          <td>2</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Sunken Pillbox(active)</td>
+          <td>4500</td>
+          <td>?</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Sunken Pillbox(inactive)</td>
+          <td>?</td>
+          <td>25</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Garrisoned House</td>
+          <td>5500</td>
+          <td>6</td>
+          <td>2</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Bunker</td>
+          <td>20500</td>
+          <td>21</td>
+          <td>5</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Watch Tower</td>
+          <td>1000</td>
+          <td>2</td>
+          <td>1</td>
+          <td>1</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Howitzer</td>
+          <td>1000</td>
+          <td>2</td>
+          <td>1</td>
+          <td>1</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Wooden Bridge</td>
+          <td>5500</td>
+          <td>6</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Stone Bridge</td>
+          <td>&gt;20000</td>
+          <td>&gt;20</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Reinforced Drawbridge</td>
+          <td>?</td>
+          <td>?</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=20 style='height:15.0pt'>
+          <td height=20 style='height:15.0pt'>Wooden Drawbridge</td>
+          <td>?</td>
+          <td>?</td>
+          <td>?</td>
+          <td>?</td>
+         </tr>
+         <tr height=0 style='display:none'>
+          <td width=180 style='width:135pt'></td>
+          <td width=75 style='width:56pt'></td>
+          <td width=75 style='width:56pt'></td>
+          <td width=75 style='width:56pt'></td>
+          <td width=75 style='width:56pt'></td>
+         </tr>
+        </table>
+        </div>
+        </body>
+        </html>
+";
+        public Data()
+        {
+            InitializeComponent();
+            webBrowser.NavigateToString(content);
+        }
+    }
+}
